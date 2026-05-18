95c47e128238a85ea226ba9fe49d095357a5921d
Jas2005-ct <jaswantjas02@gmail.com>
2026-05-18 22:59:44 +0530

    feat: implement Django admin configurations and add responsive Tailwind CSS home template

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
 
 admin.site.register(CustomUser)
 admin.site.register(Profile)
@@ -10,8 +11,16 @@ admin.site.register(Profession)
 
 @admin.register(TechStack)
 class TechStackAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'section', 'icon_preview', 'id')
+    list_filter = ('section',)
     search_fields = ('name',)
+    readonly_fields = ('icon_preview',)
+
+    @admin.display(description='Icon')
+    def icon_preview(self, obj):
+        if obj.icon:
+            return format_html('<img src="{}" alt="{}" style="height:48px;width:48px;object-fit:contain;border-radius:8px;border:1px solid #ddd;" />', obj.icon.url, obj.name)
+        return format_html('<span style="color:#999;">No icon</span>')
 
 admin.site.register(Project)
 admin.site.register(SocialLink)
